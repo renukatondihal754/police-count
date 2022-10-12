f6eac55fda259bf67a8885ffbfdda1477a8e9e6f
renukatondihal754 <111186672+renukatondihal754@users.noreply.github.com>
2022-10-12 13:10:21 +0530

    police seen theif count

--- try.py
+++ try.py
@@ -1,10 +1,14 @@
 line="PPTPPTPTP"
-police_count=0;theif_count=0
+police_count=0;
+theif_count=0;
+police_seen=False;
 for person in line:
     if person=="P":
-        police_count+=1
+        # police_count+=1
+        police_seen=True
 
-    elif person=="T":
+    elif person=="T" and police_seen:
         theif_count+=1
+        police_seen=False
 
-print(f"police={police_count} ,theif={theif_count}")
+print(f"theif={theif_count}")
